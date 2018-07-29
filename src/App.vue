33a374749ce9e4304ad7256dd4ae8b03581598d1
ShadowBan <chris.kincanon@gmail.com>
2018-07-29 16:29:11 -0700

    pushing sharebutton componet

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ShareButton></ShareButton>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
+import ShareButton from "./components/ShareButton.vue"
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    ShareButton
   }
 };
 </script>
---
+++ src/components/ShareButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click.prevent="shareThis">Hello World</button>
+</template>
+
+<script>
+export default {
+  name: "ShareButton",
+  props: {},
+  data: function(){return {}},
+  methods: {
+    shareThis: function(event) {
+      if(navigator.share){
+        navigator.share({
+          title: "Test Share",
+          text: "This is a test share from my site!",
+          url: "https://localhost:8080/test-share"
+        })
+        .then(() => console.log("share worked!"))
+        .catch((error) => console.log('Error sharing', error))
+      }else{
+        console.log("No Navigator Detected!")
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
