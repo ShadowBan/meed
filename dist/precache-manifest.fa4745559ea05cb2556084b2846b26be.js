bd788e7a61793adb5131af1b983c72b05c59b567
ShadowBan <chris.kincanon@gmail.com>
2018-07-29 23:30:38 -0700

    adding in the dist folder

---
+++ dist/precache-manifest.fa4745559ea05cb2556084b2846b26be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "970cb5f01811f8a42e23",
+    "url": "/js/chunk-vendors.970cb5f0.js"
+  },
+  {
+    "revision": "b71adead7c5e36d91db0",
+    "url": "/js/app.b71adead.js"
+  },
+  {
+    "revision": "a1c34d23852176ee4f38f1b7463e68e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71adead7c5e36d91db0",
+    "url": "/css/app.7695b329.css"
+  }
+];
