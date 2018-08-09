7272b7e8284c50b75bba46413bca57b8d7a15170
ShadowBan <chris.kincanon@gmail.com>
2018-08-08 17:59:57 -0700

    adding in webworker

---
+++ dist/precache-manifest.704b41e0e392b48ec27fd0d3ac9bebf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c0bd7e5f90d706eb243443615de8c2",
+    "url": "/share/post.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "970cb5f01811f8a42e23",
+    "url": "/js/chunk-vendors.970cb5f0.js"
+  },
+  {
+    "revision": "b71adead7c5e36d91db0",
+    "url": "/js/app.b71adead.js"
+  },
+  {
+    "revision": "f49613a371f5f60b1633a0a575a2148d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b71adead7c5e36d91db0",
+    "url": "/css/app.7695b329.css"
+  }
+];
